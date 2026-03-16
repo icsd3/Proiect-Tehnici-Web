37a2ed773740354bea0abe7a1ae3b96b17ecff23
icsd3 <urobertanton@gmail.com>
2026-03-16 15:50:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ console.log("Folder index.js", __dirname);
 console.log("Folder curent de lucru", process.cwd());
 console.log("Cale fisier", __filename);
 
-app.get("/cale/:a/:b", function(req, res)
-{
-    res.send(parseInt(req.params.a) + parseInt(req.params.b));
-    console.log("Am primit un request get pe /cale");
-});
+// app.get("/cale/:a/:b", function(req, res)
+// {
+//     res.send(parseInt(req.params.a) + parseInt(req.params.b));
+//     console.log("Am primit un request get pe /cale");
+// });
 
 // app.get("/cale2", function(req, res)
 // {
@@ -26,11 +26,11 @@ app.get("/", function(req, res)
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/:a/:b", function(req, res)
-{
-    console.log(parseInt(req.params.a) + parseInt(req.params.b));
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+// app.get("/:a/:b", function(req, res)
+// {
+//     console.log(parseInt(req.params.a) + parseInt(req.params.b));
+//     res.sendFile(path.join(__dirname, "index.html"));
+// });
 
 app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
